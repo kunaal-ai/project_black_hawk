a96650458118acead3759c3183d7bf8426ef65e3
KT <46636573+kt-SoftwareQualityAssurance@users.noreply.github.com>
2021-08-29 14:54:47 -0500

    'blac-2_interchange_first_last_elements'

---
+++ interchange_elements.py
@@ -0,0 +1,13 @@
+# Python program to interchange first and last elements in a list
+# Jira Ticket: https://blackhawk.atlassian.net/browse/BLAC-2
+
+class InterchangeElements:
+    def interchange(self, list_of_elements):
+        list_of_elements[0], list_of_elements[-1] = list_of_elements[-1], list_of_elements[0]
+        return list_of_elements
+
+
+
+obj = InterchangeElements()
+print(obj.interchange([1,2,3,4,5,6,7,8,9,0]))
+
