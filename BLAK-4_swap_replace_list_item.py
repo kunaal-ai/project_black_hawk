78ae8c96c3b66fe80de5a8935a081ab4c5d460d9
KT <46636573+kt-SoftwareQualityAssurance@users.noreply.github.com>
2021-09-18 00:40:45 -0500

    replace_join_split function

---
+++ BLAK-4_swap_replace_list_item.py
@@ -0,0 +1,13 @@
+# Python program to swap two elements with each other using replace function
+# Jira Ticket: https://blackhawk.atlassian.net/browse/BLAC-4
+
+class SwapWithEachother:
+
+    def swap_two_elements(self, user_list, element_a, element_b):
+        list_as_a_string = ", ".join(user_list)
+        replaced_and_new_list = list_as_a_string.replace(element_a, ".").replace(element_b,element_a).replace(".",element_b).split(', ')
+        print(user_list)
+        print(replaced_and_new_list)
+
+obj = SwapWithEachother()
+obj.swap_two_elements( ['Gfg', 'is', 'best', 'for', 'Geeks'], 'e','G')
